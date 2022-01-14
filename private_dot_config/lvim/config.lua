851b3277d2d3ca08bdf90564026706ff67713e22
Fábio Luciano <fabio@naoimporta.com>
2022-01-13 22:42:46 -0300

    Add Lunarvim config

---
+++ private_dot_config/lvim/config.lua
@@ -0,0 +1,133 @@
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "dracula"
+vim.opt.wrap = true
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+
+lvim.builtin.dashboard.active = true
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.show_icons.git = 1
+lvim.builtin.nvimtree.setup.view.auto_resize = true
+lvim.builtin.project.active = true
+
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "black" },
+  { command = "prettier" },
+  { command = "rustfmt" },
+  -- { command = "terraformls" },
+}
+
+-- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "pylint" },
+  { command = "eslint_d" },
+  -- { command = "terraform" },
+}
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main"
+    },
+}
+parser_configs.hcl = {
+  filetype = "hcl", "terraform",
+}
+
+-- Additional Plugins
+lvim.plugins = {
+    {
+      "dracula/vim",
+      config = function()
+        vim.g.dracula_colorterm = 0
+      end
+    },
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "BufRead",
+      config = function()
+        require "lsp_signature".setup()
+      end
+    },
+    {
+      "andymass/vim-matchup",
+      event = "CursorMoved",
+      config = function()
+        vim.g.matchup_matchparen_offscreen = { method = "popup" }
+      end,
+    },
+    {
+      "pwntester/octo.nvim",
+      event = "BufRead",
+    },
+    {
+      "simrat39/symbols-outline.nvim",
+      cmd = "SymbolsOutline",
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      event = "BufRead",
+      setup = function()
+        vim.g.indentLine_enabled = 1
+        vim.g.indent_blankline_char = "▏"
+        vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
+        vim.g.indent_blankline_buftype_exclude = {"terminal"}
+        vim.g.indent_blankline_show_trailing_blankline_indent = false
+        vim.g.indent_blankline_show_first_indent_level = false
+      end
+    },
+    {
+      "folke/todo-comments.nvim",
+      event = "BufRead",
+      config = function()
+        require("todo-comments").setup()
+      end,
+    },
+    {
+      "nvim-neorg/neorg",
+      config = function()
+          require('neorg').setup {
+              -- Tell Neorg what modules to load
+              load = {
+                  ["core.defaults"] = {}, -- Load all the default modules
+                  ["core.norg.concealer"] = {}, -- Allows for use of icons
+                  ["core.norg.dirman"] = { -- Manage your directories with Neorg
+                      config = {
+                          workspaces = {
+                              my_workspace = "~/neorg"
+                          }
+                      }
+                  }
+              },
+          }
+      end,
+      requires = "nvim-lua/plenary.nvim"
+    }
+}
+
