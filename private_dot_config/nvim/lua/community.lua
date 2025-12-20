0f37fb3efd0ba40b4de1679a54db00f4a13efa3a
Fábio Luciano <fabio@naoimporta.com>
2025-12-20 01:23:05 -0300

    add-things

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -5,181 +5,190 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
+	"AstroNvim/astrocommunity",
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Appearance                          │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.color.twilight-nvim" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      on_highlights = function(hl, c)
-        hl.SpellBad = { bg = "#3d2026", fg = "#f7768e", underline = true }
-        hl.SpellCap = { bg = "#3d3520", fg = "#e0af68", underline = true }
-        hl.SpellLocal = { bg = "#203040", fg = "#7aa2f7", underline = true }
-        hl.SpellRare = { bg = "#302040", fg = "#bb9af7", underline = true }
-      end,
-    },
-  },
-  { import = "astrocommunity.bars-and-lines.dropbar-nvim" },
-  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Appearance                          │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.color.twilight-nvim" },
+	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			on_highlights = function(hl, c)
+				hl.SpellBad = { bg = "#3d2026", fg = "#f7768e", underline = true }
+				hl.SpellCap = { bg = "#3d3520", fg = "#e0af68", underline = true }
+				hl.SpellLocal = { bg = "#203040", fg = "#7aa2f7", underline = true }
+				hl.SpellRare = { bg = "#302040", fg = "#bb9af7", underline = true }
+			end,
+		},
+	},
+	{ import = "astrocommunity.bars-and-lines.dropbar-nvim" },
+	{ import = "astrocommunity.bars-and-lines.vim-illuminate" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Completion                          │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Completion                          │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.completion.codeium-vim" },
+	{ import = "astrocommunity.completion.copilot-lua-cmp" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Code Runner                         │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.code-runner.overseer-nvim" },
-  { import = "astrocommunity.code-runner.sniprun" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Code Runner                         │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.code-runner.overseer-nvim" },
+	{ import = "astrocommunity.code-runner.sniprun" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Debugging                           │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
-  { import = "astrocommunity.debugging.telescope-dap-nvim" },
-  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Debugging                           │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+	{ import = "astrocommunity.debugging.telescope-dap-nvim" },
+	{ import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Diagnostics                         │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Diagnostics                         │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.diagnostics.lsp_lines-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Editing Support                     │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.editing-support.comment-box-nvim" },
-  { import = "astrocommunity.editing-support.conform-nvim" },
-  { import = "astrocommunity.editing-support.nvim-regexplainer" },
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
-  { import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
-  { import = "astrocommunity.editing-support.vim-move" },
-  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-  { import = "astrocommunity.editing-support.yanky-nvim" },
-  { import = "astrocommunity.editing-support.refactoring-nvim" },
-  { import = "astrocommunity.editing-support.zen-mode-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Editing Support                     │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.editing-support.comment-box-nvim" },
+	{ import = "astrocommunity.editing-support.conform-nvim" },
+	{ import = "astrocommunity.editing-support.nvim-regexplainer" },
+	{ import = "astrocommunity.editing-support.todo-comments-nvim" },
+	{ import = "astrocommunity.editing-support.ultimate-autopair-nvim" },
+	{ import = "astrocommunity.editing-support.vim-move" },
+	{ import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+	{ import = "astrocommunity.editing-support.yanky-nvim" },
+	{ import = "astrocommunity.editing-support.refactoring-nvim" },
+	{ import = "astrocommunity.editing-support.zen-mode-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    File Explorer                       │
-  -- ╰────────────────────────────────────────────────────────╯
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    File Explorer                       │
+	-- ╰────────────────────────────────────────────────────────╯
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Fuzzy Finder                        │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Fuzzy Finder                        │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Git                                 │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.git.blame-nvim" },
-  { import = "astrocommunity.git.octo-nvim" },
-  { import = "astrocommunity.git.neogit" },
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.git-blame-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Git                                 │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.git.blame-nvim" },
+	{ import = "astrocommunity.git.octo-nvim" },
+	{ import = "astrocommunity.git.neogit" },
+	{ import = "astrocommunity.git.diffview-nvim" },
+	{ import = "astrocommunity.git.git-blame-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Indent                              │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.indent.mini-indentscope" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Indent                              │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.indent.mini-indentscope" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    LSP                                 │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.lsp.nvim-lint" },
-  { import = "astrocommunity.lsp.lspsaga-nvim" },
-  { import = "astrocommunity.lsp.garbage-day-nvim" },
-  { import = "astrocommunity.lsp.inc-rename-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    LSP                                 │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.lsp.nvim-lint" },
+	{ import = "astrocommunity.lsp.lspsaga-nvim" },
+	{ import = "astrocommunity.lsp.garbage-day-nvim" },
+	{ import = "astrocommunity.lsp.inc-rename-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Markdown & LaTeX                    │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
-  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Markdown & LaTeX                    │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.markdown-and-latex.glow-nvim" },
+	{ import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+	{ import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Motion                              │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.motion.flash-nvim" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.hop-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Motion                              │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.motion.flash-nvim" },
+	{ import = "astrocommunity.motion.nvim-surround" },
+	{ import = "astrocommunity.motion.mini-move" },
+	{ import = "astrocommunity.motion.hop-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Language Packs                      │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.pack.angular" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.helm" },
-  { import = "astrocommunity.pack.html-css" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.php" },
-  { import = "astrocommunity.pack.pkl" },
-  { import = "astrocommunity.pack.python-ruff" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.sql" },
-  { import = "astrocommunity.pack.terraform" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.xml" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.chezmoi" },
-  { import = "astrocommunity.pack.cmake" },
-  { import = "astrocommunity.pack.nix" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Language Packs                      │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.pack.angular" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.helm" },
+	{ import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.java" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.php" },
+	{ import = "astrocommunity.pack.pkl" },
+	{ import = "astrocommunity.pack.python-ruff" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.sql" },
+	{ import = "astrocommunity.pack.terraform" },
+	{ import = "astrocommunity.pack.toml" },
+	{ import = "astrocommunity.pack.typescript-all-in-one" },
+	{ import = "astrocommunity.pack.vue" },
+	{ import = "astrocommunity.pack.xml" },
+	{ import = "astrocommunity.pack.yaml" },
+	{ import = "astrocommunity.pack.chezmoi" },
+	{ import = "astrocommunity.pack.cmake" },
+	{ import = "astrocommunity.pack.nix" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Project                             │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.project.project-nvim" },
-  -- { import = "astrocommunity.project.nvim-spectre" }, -- DISABLED: module not found
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Project                             │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.project.project-nvim" },
+	-- { import = "astrocommunity.project.nvim-spectre" }, -- DISABLED: module not found
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Recipes                             │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.recipes.vscode" },
-  { import = "astrocommunity.recipes.vscode-icons" },
-  { import = "astrocommunity.recipes.neovide" },
-  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Recipes                             │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.recipes.vscode" },
+	{ import = "astrocommunity.recipes.vscode-icons" },
+	{ import = "astrocommunity.recipes.neovide" },
+	{ import = "astrocommunity.recipes.telescope-lsp-mappings" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Scrolling                           │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.scrolling.neoscroll-nvim" },
-  { import = "astrocommunity.scrolling.mini-animate" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Scrolling                           │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.scrolling.neoscroll-nvim" },
+	{ import = "astrocommunity.scrolling.mini-animate" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Syntax                              │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.syntax.vim-easy-align" },
-  { import = "astrocommunity.syntax.hlargs-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Syntax                              │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.syntax.vim-easy-align" },
+	{ import = "astrocommunity.syntax.hlargs-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Terminal Integration                │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.terminal-integration.vim-tmux-yank" },
-  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
-  { import = "astrocommunity.terminal-integration.flatten-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Terminal Integration                │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.terminal-integration.vim-tmux-yank" },
+	{ import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+	{ import = "astrocommunity.terminal-integration.flatten-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Utility                             │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Utility                             │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.utility.noice-nvim" },
+	{ import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
 
-  -- ╭────────────────────────────────────────────────────────╮
-  -- │                    Workflow                            │
-  -- ╰────────────────────────────────────────────────────────╯
-  { import = "astrocommunity.workflow.precognition-nvim" },
-
+	-- ╭────────────────────────────────────────────────────────╮
+	-- │                    Workflow                            │
+	-- ╰────────────────────────────────────────────────────────╯
+	{ import = "astrocommunity.workflow.precognition-nvim" },
+	{ import = "astrocommunity.split-and-window.minimap-vim" },
+	{
+		"wfxr/minimap.vim",
+		lazy = false,
+		config = function()
+			vim.g.minimap_width = 10
+			vim.g.minimap_auto_start = 1
+			vim.g.minimap_auto_start_win_enter = 1
+		end,
+	},
 }
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -4,164 +4,202 @@
 -- ╰──────────────────────────────────────────────────────────╯
 
 return {
-  -- Justfile syntax
-  { "NoahTheDuke/vim-just", ft = "just" },
+	-- Justfile syntax
+	{ "NoahTheDuke/vim-just", ft = "just" },
 
-  -- Discord presence
-  {
-    "andweeb/presence.nvim",
-    event = "VeryLazy",
-    opts = {
-      neovim_image_text = "The One True Text Editor",
-      main_image = "neovim",
-      debounce_timeout = 10,
-      enable_line_number = false,
-      buttons = true,
-    },
-  },
+	-- Discord presence
+	{
+		"andweeb/presence.nvim",
+		event = "VeryLazy",
+		opts = {
+			neovim_image_text = "The One True Text Editor",
+			main_image = "neovim",
+			debounce_timeout = 10,
+			enable_line_number = false,
+			buttons = true,
+		},
+	},
 
-  -- LSP signature help
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "LspAttach",
-    opts = {
-      bind = true,
-      handler_opts = { border = "rounded" },
-      floating_window = true,
-      hint_enable = true,
-      hint_prefix = "󰏫 ",
-      hi_parameter = "LspSignatureActiveParameter",
-    },
-  },
+	-- LSP signature help
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "LspAttach",
+		opts = {
+			bind = true,
+			handler_opts = { border = "rounded" },
+			floating_window = true,
+			hint_enable = true,
+			hint_prefix = "󰏫 ",
+			hi_parameter = "LspSignatureActiveParameter",
+		},
+	},
 
-  -- LuaSnip configuration
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts)
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-      luasnip.filetype_extend("typescript", { "typescriptreact" })
-      -- Load friendly-snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
+	-- LuaSnip configuration
+	{
+		"L3MON4D3/LuaSnip",
+		config = function(plugin, opts)
+			require("astronvim.plugins.configs.luasnip")(plugin, opts)
+			local luasnip = require("luasnip")
+			luasnip.filetype_extend("javascript", { "javascriptreact" })
+			luasnip.filetype_extend("typescript", { "typescriptreact" })
+			-- Load friendly-snippets
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
 
-  -- Better quick fix list
-  {
-    "kevinhwang91/nvim-bqf",
-    ft = "qf",
-    opts = {
-      preview = {
-        winblend = 0,
-      },
-    },
-  },
+	-- Better quick fix list
+	{
+		"kevinhwang91/nvim-bqf",
+		ft = "qf",
+		opts = {
+			preview = {
+				winblend = 0,
+			},
+		},
+	},
 
-  -- Better marks
-  {
-    "chentoast/marks.nvim",
-    event = "BufReadPost",
-    opts = {
-      default_mappings = true,
-      signs = true,
-      mappings = {},
-    },
-  },
+	-- Better marks
+	{
+		"chentoast/marks.nvim",
+		event = "BufReadPost",
+		opts = {
+			default_mappings = true,
+			signs = true,
+			mappings = {},
+		},
+	},
 
-  -- Undo tree visualization
-  {
-    "mbbill/undotree",
-    cmd = "UndotreeToggle",
-    keys = {
-      { "<leader>U", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
-    },
-  },
+	-- Undo tree visualization
+	{
+		"mbbill/undotree",
+		cmd = "UndotreeToggle",
+		keys = {
+			{ "<leader>U", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
+		},
+	},
 
-  -- Color highlighter
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      filetypes = { "*" },
-      user_default_options = {
-        RGB = true,
-        RRGGBB = true,
-        names = false,
-        RRGGBBAA = true,
-        css = true,
-        css_fn = true,
-        mode = "virtualtext",
-        virtualtext = "■",
-        tailwind = true,
-      },
-    },
-  },
+	-- Color highlighter
+	{
+		"NvChad/nvim-colorizer.lua",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			filetypes = { "*" },
+			user_default_options = {
+				RGB = true,
+				RRGGBB = true,
+				names = false,
+				RRGGBBAA = true,
+				css = true,
+				css_fn = true,
+				mode = "virtualtext",
+				virtualtext = "■",
+				tailwind = true,
+			},
+		},
+	},
 
-  -- Better fold
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    event = "BufReadPost",
-    opts = {
-      provider_selector = function() return { "treesitter", "indent" } end,
-    },
-    init = function()
-      vim.o.foldcolumn = "1"
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-    end,
-    keys = {
-      { "zR", function() require("ufo").openAllFolds() end, desc = "Open all folds" },
-      { "zM", function() require("ufo").closeAllFolds() end, desc = "Close all folds" },
-      { "zK", function() require("ufo").peekFoldedLinesUnderCursor() end, desc = "Peek fold" },
-    },
-  },
+	-- Better fold
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		event = "BufReadPost",
+		opts = {
+			provider_selector = function()
+				return { "treesitter", "indent" }
+			end,
+		},
+		init = function()
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+		end,
+		keys = {
+			{
+				"zR",
+				function()
+					require("ufo").openAllFolds()
+				end,
+				desc = "Open all folds",
+			},
+			{
+				"zM",
+				function()
+					require("ufo").closeAllFolds()
+				end,
+				desc = "Close all folds",
+			},
+			{
+				"zK",
+				function()
+					require("ufo").peekFoldedLinesUnderCursor()
+				end,
+				desc = "Peek fold",
+			},
+		},
+	},
 
-  -- Session management
-  {
-    "folke/persistence.nvim",
-    event = "BufReadPre",
-    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
-    keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load { last = true } end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Session" },
-    },
-  },
+	-- Session management
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre",
+		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+		keys = {
+			{
+				"<leader>qs",
+				function()
+					require("persistence").load()
+				end,
+				desc = "Restore Session",
+			},
+			{
+				"<leader>ql",
+				function()
+					require("persistence").load({ last = true })
+				end,
+				desc = "Restore Last Session",
+			},
+			{
+				"<leader>qd",
+				function()
+					require("persistence").stop()
+				end,
+				desc = "Don't Save Session",
+			},
+		},
+	},
 
-  -- Smooth cursor
-  {
-    "gen740/SmoothCursor.nvim",
-    event = "VeryLazy",
-    opts = {
-      type = "default",
-      fancy = {
-        enable = true,
-        head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil },
-      },
-      speed = 25,
-      intervals = 35,
-    },
-  },
+	-- Smooth cursor
+	{
+		"gen740/SmoothCursor.nvim",
+		event = "VeryLazy",
+		opts = {
+			type = "default",
+			fancy = {
+				enable = true,
+				head = { cursor = "▷", texthl = "SmoothCursor", linehl = nil },
+			},
+			speed = 25,
+			intervals = 35,
+		},
+	},
 
-  -- Claude Code integration
-  {
-    "greggh/claude-code.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>C", group = "Claude" },
-      { "<leader>Cc", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude Code" },
-      { "<leader>Cs", "<cmd>ClaudeCodeSend<cr>", desc = "Send to Claude", mode = { "n", "v" } },
-    },
-    opts = {
-      window = {
-        position = "float", -- Janela flutuante
-        enter_insert = true,
-      },
-    },
-  },
+	-- Claude Code integration
+	{
+		"greggh/claude-code.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>C", group = "Claude" },
+			{ "<leader>Cc", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude Code" },
+			{ "<leader>Cs", "<cmd>ClaudeCodeSend<cr>", desc = "Send to Claude", mode = { "n", "v" } },
+		},
+		opts = {
+			window = {
+				position = "float", -- Janela flutuante
+				enter_insert = true,
+			},
+		},
+	},
 }
